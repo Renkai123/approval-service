68c4979c77f72d134ae59eda039f5da259af36f3
renkai <Kren@91souban.com>
2024-04-08 16:53:34 +0800

    123

---
+++ src/test/java/me/ricky/approvalservice/ApprovalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ricky.approvalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprovalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
